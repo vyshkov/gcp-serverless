369fcb5ff92a48d64f1f9c9b5cff7b749e7d3b6b
Volodymyr Vyshko <volodymyr.vyshko@globallogic.com>
2023-02-25 22:26:00 +0200

    BACK: added sample service

---
+++ backend/service-test/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// [START functions_http_method]
+const functions = require('@google-cloud/functions-framework');
+
+/**
+ * Responds to a GET request with "Hello World!". Forbids a PUT request.
+ *
+ * @example
+ * gcloud functions call helloHttp
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+functions.http('helloHttp', (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      res.status(200).send('Hello World!');
+      break;
+    case 'PUT':
+      res.status(403).send('Forbidden!');
+      break;
+    default:
+      res.status(405).send({error: 'Something blew up!'});
+      break;
+  }
+});
+// [END functions_http_method]
