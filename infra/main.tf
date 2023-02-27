745f9b1e96c6ef0e98a5ab64f3493ffa734708a1
Volodymyr Vyshko <volodymyr.vyshko@globallogic.com>
2023-02-27 23:32:29 +0200

    CHORE: rename function

--- infra/main.tf
+++ infra/main.tf
@@ -6,6 +6,6 @@ module "function_test_service" {
 
 module "function_test_service_2" {
   source = "./modules/function"
-  function_name = "http_test1"
-  function_description = "http_test1 desc"
+  function_name = "http_test2"
+  function_description = "http_test2 desc"
 }
