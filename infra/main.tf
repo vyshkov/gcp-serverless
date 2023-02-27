cb488ae61a9ca96b3682e82e22205066de86a42f
Volodymyr Vyshko <volodymyr.vyshko@globallogic.com>
2023-02-27 23:34:11 +0200

    CHORE: update resource name

--- infra/main.tf
+++ infra/main.tf
@@ -1,11 +1,11 @@
 module "function_test_service" {
   source = "./modules/function"
-  function_name = "http_test1"
+  function_name = "httptest1"
   function_description = "http_test1 desc"
 }
 
 module "function_test_service_2" {
   source = "./modules/function"
-  function_name = "http_test2"
+  function_name = "httptest2"
   function_description = "http_test2 desc"
 }
