223fa6bcb15f40dbae0879c440503d9f658e9d48
Volodymyr <vovanok1992@gmail.com>
2023-03-11 15:40:33 +0200

    FRONT: updated client app to use material ui

--- frontend/client-app/src/App.tsx
+++ frontend/client-app/src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-
-//import { CredentialResponse, useGoogleOneTapLogin } from '@react-oauth/google';
-
-import './App.css';
 import { useGoogleOneTapLogin } from './auth/useLogin';
+
+import { AppBar, Avatar, Box, CircularProgress, Container, IconButton, Toolbar, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
 
 const dev = false;
 
@@ -12,31 +11,49 @@ const LOCAL_API_PATH = "http://localhost:8080"
 
 const API_PATH = dev ? LOCAL_API_PATH : GW_API_PATH;
 
+function parseJwt (token: string) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
+
+type User = {
+  given_name: string;
+  family_name: string;
+  picture: string;
+}
+  
 function App() {
-
-  const [data, setData] = useState("-");
+  const [data, setData] = useState<User>();
   const [token, setToken] = useState<string | undefined | null>(localStorage.getItem("credential"));
-  console.log('current token', token)
+  const [isInProgress, setInProgress] = useState(false);
 
+  const userData = token && parseJwt(token) || {};
+  console.log('current token', userData);
+  console.log('current data', data);
 
   useEffect(() => {
     if (token) {
-
       const jwtPayload = JSON.parse(window.atob(token.split('.')[1]))
       console.log(jwtPayload.exp);
       if (Date.now() >= jwtPayload.exp * 1000) {
         localStorage.clear();
         setToken(undefined);
       } else {
-        setData("...");
+        setInProgress(true);
         fetch(`${API_PATH}/hello2`, {
           headers: {
             Authorization: `Bearer ${token}`,
           }
         })
-        .then(resp => resp.text())
-        .then(resp => setData(resp || 'no'))
-        .catch(err => setData(err.errorMessage));
+        .then(resp => resp.json())
+        .then(resp => setData(resp || {}))
+        .catch(err => setData(err.errorMessage))
+        .finally(() => setInProgress(false));
       }
     }
   }, [token])
@@ -45,18 +62,38 @@ function App() {
     client_id: "736194043976-ks3e2r68img0ldda4danrbo9j9olvjf3.apps.googleusercontent.com",
     disabled: Boolean(token),
     callback: ({ credential }) => {
-        console.log('credential', credential);
-        setToken(credential);
-        localStorage.setItem('credential', credential);
+      console.log('credential', credential);
+      setToken(credential);
+      localStorage.setItem('credential', credential);
     }
-  })
+  });
 
   return (
-    <div className="App">
-      <div>
-        Secured API response: {data}
-      </div>
-    </div>
+    <Box>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant="h6" sx={{flexGrow: 1 }}>My App</Typography>
+            {token ? (
+            <Stack sx={{ flexDirection: "row" }}>
+              <Typography variant="h6" sx={{ paddingRight: 2 }}>{userData.given_name}</Typography>
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt={userData.given_name} src={userData.picture} />
+              </IconButton>
+            </Stack>
+            
+            ) : (
+              <Typography variant="h6">Not logged in</Typography>
+            )}
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="lg" sx={{ padding: 3 }}>
+        {isInProgress ? (
+          <CircularProgress />
+        ) : (
+          <Typography variant="h6">Secured API response: {data?.given_name || "-"}</Typography>
+        )}
+      </Container>
+    </Box>
   );
 }
 
--- frontend/client-app/src/index.tsx
+++ frontend/client-app/src/index.tsx
@@ -3,16 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
- 
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
 
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
