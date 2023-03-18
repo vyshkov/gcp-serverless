3dd87f4617240e2a9626e4e463e0880dd32cd7f2
Volodymyr <vovanok1992@gmail.com>
2023-03-18 18:43:06 +0200

    BE: updated test service

--- backend/service-test/index.js
+++ backend/service-test/index.js
@@ -1,28 +1,32 @@
-'use strict';
-
-// [START functions_http_method]
+const axios = require('axios');
 const functions = require('@google-cloud/functions-framework');
+const cors = require('cors')({ origin: true });
 
-/**
- * Responds to a GET request with "Hello World!". Forbids a PUT request.
- *
- * @example
- * gcloud functions call helloHttp
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
 functions.http('helloHttp', (req, res) => {
-  switch (req.method) {
-    case 'GET':
-      res.status(200).sendFile('./static/index.html', { root: __dirname });
-      break;
-    case 'PUT':
-      res.status(403).send('Forbidden!');
-      break;
-    default:
-      res.status(405).send({error: 'Something blew up!'});
-      break;
-  }
+  cors(req, res, async () => {
+
+    // GCP cloud function wich should trigger GPT API to generate random quote
+    // and return it to the client
+    try {
+      // Make a POST request to the GPT API endpoint
+      const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-002/completions', {
+        prompt: `Write a 2Pac quote about life:`,
+        max_tokens: 50,
+        temperature: 0.7,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer XXX`,
+        },
+      });
+  
+      // Extract the generated quote from the response
+      const generatedQuote = response.data.choices[0].text.trim();
+  
+      res.status(200).json({ quote: generatedQuote, author: "GPT" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Internal server error: ' + error });
+    }
+  })
 });
-// [END functions_http_method]
