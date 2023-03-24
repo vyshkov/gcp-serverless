cd6ebc3263f3d37182b36fe01f79312bc3d8e338
Volodymyr <vovanok1992@gmail.com>
2023-03-24 15:09:03 +0200

    FE: updated table

--- frontend/client-app/src/components/Table.tsx
+++ frontend/client-app/src/components/Table.tsx
@@ -7,7 +7,18 @@ import Paper from '@mui/material/Paper';
 
 import AddIcon from '@mui/icons-material/Add'
 
-import { Box, CircularProgress, IconButton, InputAdornment, ListItemIcon, ListItemText, Menu, MenuItem, OutlinedInput } from '@mui/material';
+import {
+    Backdrop,
+    Box, 
+    CircularProgress, 
+    IconButton, 
+    InputAdornment, 
+    ListItemIcon, 
+    ListItemText, 
+    Menu, 
+    MenuItem, 
+    OutlinedInput
+} from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useAuth } from '../auth/useLogin';
 import Typography from '@mui/material/Typography';
@@ -19,6 +30,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 
 interface Word {
+    id: string;
     word: string;
     translation: string;
 }
@@ -34,25 +46,26 @@ export default function BasicTable() {
     const [words, setWords] = useState<Word[]>([]);
     const [inProgress, setInProgress] = useState(false);
     const [isWordDialogOpen, setIsDialogOpen] = useState(false);
+    const [isUpdating, setIsUpdating] = useState(false);
 
-    const [selected, setSelected] = useState<string | null>(null);
+    const [selected, setSelected] = useState<Word | null>(null);
     const [contextMenu, setContextMenu] = useState<{
         mouseX: number;
         mouseY: number;
-      } | null>(null);
+    } | null>(null);
 
     const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
         setContextMenu(
             contextMenu === null
-              ? {
-                  mouseX: event.clientX + 2,
-                  mouseY: event.clientY - 6,
+                ? {
+                    mouseX: event.clientX + 2,
+                    mouseY: event.clientY - 6,
                 }
-              : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+                : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
                 // Other native context menus might behave different.
                 // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
                 null,
-          );
+        );
     };
 
     const handleClose = () => {
@@ -95,6 +108,15 @@ export default function BasicTable() {
             .finally(() => setInProgress(false));
     }
 
+    const deleteWord = () => {
+        setIsUpdating(true);
+        console.log(selected)
+        setTimeout(() => {
+            setIsUpdating(false)
+            handleClose();
+        }, 1000);
+    }
+
     useEffect(() => {
         if (token) {
             reload(token, setIsUserAllowed);
@@ -103,17 +125,24 @@ export default function BasicTable() {
 
     return (
         <Stack sx={{ flex: 1, display: "flex", justifyContent: "flex-start", alignItems: "center", width: 1, padding: 0, minHeight: 0 }}>
+            <Backdrop
+                sx={{ color: '#fff' }}
+                open={isUpdating}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
             <Box sx={{ width: 1, px: 2, pt: 3 }}>
                 <OutlinedInput
                     placeholder='Search...'
                     sx={{ width: 1 }}
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
-                    disabled={inProgress}
+                    disabled={inProgress || isUpdating}
                     endAdornment={
                         <InputAdornment position="end">
                             <IconButton
                                 aria-label="toggle password visibility"
+                                disabled={inProgress || isUpdating}
                                 onClick={handleAddWordOpen}
                                 edge="end"
                             >
@@ -132,16 +161,19 @@ export default function BasicTable() {
                         No words found
                     </Typography>
                 ) : (
-                    <TableContainer component={Paper} sx={{ px: 1, pt: 2, flex: 1, background: "transparent", boxShadow: "none", overflow: "auto" }}>
-                        <Table aria-label="simple table">
+                    <TableContainer
+                        component={Paper}
+                        sx={{ px: 1, pt: 2, flex: 1, background: "transparent", boxShadow: "none", overflow: "auto" }}
+                    >
+                        <Table>
                             <TableBody>
                                 {words.filter(el => wordMatchesSearch(el, search)).map((row) => (
                                     <TableRow
-                                        key={row.word}
-                                        sx={{ '&:last-child td, &:last-child th': { border: 0 }, background: selected === row.word ? "rgba(0,0,0,0.5)" : "transparent" }}
+                                        key={row.id}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 }, background: selected?.id === row.id ? "rgba(0,0,0,0.5)" : "transparent" }}
                                         onClick={(evt: React.MouseEvent<HTMLElement>) => {
                                             handleClickListItem(evt);
-                                            setSelected(row.word);
+                                            setSelected(row);
                                         }}
                                     >
                                         <TableCell component="th" scope="row" sx={{ fontWeight: "bold", letterSpacing: 1 }}>
@@ -161,19 +193,19 @@ export default function BasicTable() {
                 anchorReference="anchorPosition"
                 anchorPosition={
                     contextMenu !== null
-                      ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
-                      : undefined
-                  }
-              
+                        ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+                        : undefined
+                }
+
                 onClose={handleClose}
                 MenuListProps={{
                     'aria-labelledby': 'lock-button',
                     role: 'listbox',
                 }}
             >
-
                 <MenuItem
-                    onClick={() => alert(selected)}
+                    onClick={deleteWord}
+                    disabled={isUpdating}
                 >
                     <ListItemIcon>
                         <DeleteIcon fontSize="small" />
@@ -181,7 +213,7 @@ export default function BasicTable() {
                     <ListItemText>Delete</ListItemText>
                 </MenuItem>
                 <MenuItem
-                    onClick={() => alert(selected)}
+                    disabled
                 >
                     <ListItemIcon>
                         <EditIcon fontSize="small" />
