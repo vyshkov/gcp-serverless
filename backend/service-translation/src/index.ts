f2cdfbb49e1c69547afde8e0eb4aca7f4b60f85a
Volodymyr Vyshko <volodymyr.vyshko@globallogic.com>
2023-03-29 00:47:48 +0300

    BE: added fastify

---
+++ backend/service-translation/src/index.ts
@@ -0,0 +1,39 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+const functions = require('@google-cloud/functions-framework');
+const escapeHtml = require('escape-html');
+const jwt = require('jsonwebtoken');
+const Firestore = require('@google-cloud/firestore');
+
+const cors = require('cors')({ origin: true });
+
+const COLLECTION_NAME = 'users';
+
+const firestore = new Firestore({
+    timestampsInSnapshots: true
+    // NOTE: Don't hardcode your project credentials here.
+    // If you have to, export the following to your shell:
+    //   GOOGLE_APPLICATION_CREDENTIALS=<path>
+    // keyFilename: '/cred/cloud-functions-firestore-000000000000.json',
+});
+
+const fastify = require("fastify")({
+    logger: true // you can also define the level passing an object configuration to logger: {level: 'debug'}
+});
+
+fastify.addContentTypeParser('application/json', {}, (req: FastifyRequest, body: any, done: any) => {
+    done(null, body.body);
+});
+
+fastify.get('/', async (request: FastifyRequest , reply: FastifyReply) => {
+    return { hello: 'world123' }
+})
+
+const fastifyFunction = async (request: FastifyRequest , reply: FastifyReply) => {
+    await fastify.ready();
+    fastify.server.emit('request', request, reply)
+}
+
+module.exports = {
+    fastifyFunction
+}
