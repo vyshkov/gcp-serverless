da1855112e5c09f56dc1289f4071c5c84f17b17a
Volodymyr <vovanok1992@gmail.com>
2023-04-01 18:54:26 +0300

    BE: updated fastify service

--- backend/service-translation/src/index.ts
+++ backend/service-translation/src/index.ts
@@ -1,11 +1,11 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from "fastify";
 
 const functions = require('@google-cloud/functions-framework');
-const escapeHtml = require('escape-html');
 const jwt = require('jsonwebtoken');
 const Firestore = require('@google-cloud/firestore');
 
-const cors = require('cors')({ origin: true });
+import cors from '@fastify/cors'
+import { QuerySnapshot } from "@google-cloud/firestore";
 
 const COLLECTION_NAME = 'users';
 
@@ -21,12 +21,51 @@ const fastify = require("fastify")({
     logger: true // you can also define the level passing an object configuration to logger: {level: 'debug'}
 });
 
+fastify.register(cors, {
+    // put your options here
+});
+
+fastify.addHook('preHandler', async (request: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) => {
+    let token = request.headers['x-forwarded-authorization'] || request.headers['authorization'];
+    console.log('token', request.headers)
+
+    if (!token) {
+        reply.code(401).send({ message: 'Unauthorized' });
+        return;
+    }
+
+    const tokenParts = (token as string).split(' ');
+
+    try {
+      const decodedToken = jwt.decode(tokenParts[1], { complete: true });
+      const email = decodedToken.payload.email;
+
+      const querySnapshot: QuerySnapshot = await firestore
+        .collection(COLLECTION_NAME).where('email', '==', email).get();
+        
+      if (!querySnapshot.empty) {
+        const user = querySnapshot.docs[0].data();
+        console.log('???', user);
+        request.headers['x-user-email'] = email;
+        request.headers['x-user-id'] = user.id;
+        request.headers['x-user-roles'] = JSON.stringify(user.roles);
+        done();
+      } else {
+        reply.code(403).send({ message: 'The user is not in the database' });
+        return;
+      }
+    } catch (err) {
+        reply.code(500).send({ message: 'Failed to check auth' });
+        return;
+    }
+});
+
 fastify.addContentTypeParser('application/json', {}, (req: FastifyRequest, body: any, done: any) => {
     done(null, body.body);
 });
 
-fastify.get('/', { prefix: "/test" }, async (request: FastifyRequest , reply: FastifyReply) => {
-    return { hello: '4' }
+fastify.get('/', async (request: FastifyRequest , reply: FastifyReply) => {
+    return { hello: request.headers }
 });
 
 const main = async (request: FastifyRequest , reply: FastifyReply) => {
--- frontend/client-app/src/components/Layout.tsx
+++ frontend/client-app/src/components/Layout.tsx
@@ -28,6 +28,16 @@ const Layout = () => {
                 }
             }, 500)
         }
+
+        fetch('http://localhost:8080', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+
     }, [token, renderLoginButton, refContainer]);
 
     return (
