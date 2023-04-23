15c40a69017fc3ae261b3df96115be7b693e9822
Volodymyr <vovanok1992@gmail.com>
2023-04-23 22:51:46 +0300

    FE: updates suggestions

--- frontend/client-app/src/components/dictionary/Suggestions.tsx
+++ frontend/client-app/src/components/dictionary/Suggestions.tsx
@@ -24,12 +24,12 @@ interface SuggestionsProps {
     onFreeDictionaryWordClick: (definitions: DictionaryEntry[]) => void;
 }
 
-const Suggestions = ({ 
-    word, 
+const Suggestions = ({
+    word,
     translate,
-    onTranslationPressed, 
-    onUrbanDictionaryWordClick, 
-    onFreeDictionaryWordClick 
+    onTranslationPressed,
+    onUrbanDictionaryWordClick,
+    onFreeDictionaryWordClick
 }: SuggestionsProps) => {
     const client = useClient();
     const previousController = useRef<AbortController>();
@@ -55,31 +55,15 @@ const Suggestions = ({
 
                 setInProgress(true);
                 client.translateAzure(
-                    word, 
-                    isEnglish ? "en" : "uk", 
-                    isEnglish ? "uk" : "en", 
+                    word,
+                    isEnglish ? "en" : "uk",
+                    isEnglish ? "uk" : "en",
                     abortController
                 )
-                .then((res: TranslationObject[]) => {
-                    setTranslationResults(res[0].translations.map(el => el.text));
-                 })
-                .finally(() => setInProgress(false));
-
-                fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=uk&hl=en&dt=t&dt=bd&dj=1&source=icon&tk=215202.215202&q=${word}`, {
-                        signal: abortController.signal
-                })
-                    .then(res => res.json())
-                    .then(res => {
-                        console.log("Goog1", res);
-                        if (res?.dict?.length > 0) {
-                            return res.dict.flatMap((el: any) => el.terms);
-                        }
-                        return [];
+                    .then((res: TranslationObject[]) => {
+                        setTranslationResults(res[0].translations.map(el => el.text));
                     })
-                    .then(res => {
-                        setGoogleTranslationResults(res);
-                    })
-                    .catch(err => console.log("Goog", err));
+                    .finally(() => setInProgress(false));
 
                 // client.translateGoogle(
                 //     word,
@@ -91,9 +75,27 @@ const Suggestions = ({
                 // })
             }
 
+
+            fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=uk&hl=en&dt=t&dt=bd&dj=1&source=icon&tk=215202.215202&q=${word}`, {
+                signal: abortController.signal
+            })
+                .then(res => res.json())
+                .then(res => {
+                    console.log("Goog1", res);
+                    if (res?.dict?.length > 0) {
+                        return res.dict.flatMap((el: any) => el.terms);
+                    }
+                    return [];
+                })
+                .then(res => {
+                    setGoogleTranslationResults(res);
+                })
+                .catch(err => console.log("Goog", err));
+
+
             getFreeDefinition(debouncedSearch, previousController?.current)
                 .then(res => setFreeDefinitions(res));
-            
+
             getUrbanDictionaryDefinition(debouncedSearch, previousController?.current)
                 .then(res => setUrbanDictionaryDefinitions(res));
         }
@@ -107,35 +109,35 @@ const Suggestions = ({
 
             {Boolean(googleTranslationResults.length) && googleTranslationResults.map((el, i) => (
                 // eslint-disable-next-line react/no-array-index-key
-                <Chip key={el + i} label={el} onClick={() => onTranslationPressed(word, el)} 
+                <Chip key={el + i} label={el} onClick={() => onTranslationPressed(word, el)}
                     sx={{ mr: 1, mb: 1, background: theme.custom?.transparentLight }} />
             ))}
 
             {translate && !showLoading && translationResults.map((el, i) => (
                 // eslint-disable-next-line react/no-array-index-key
-                <Chip key={el + i} label={el} onClick={() => onTranslationPressed(word, el)} 
+                <Chip key={el + i} label={el} onClick={() => onTranslationPressed(word, el)}
                     sx={{ mr: 1, mb: 1 }} />
             ))}
             {freeDefinitions?.slice(0, 1)?.map((def, i) => (
-                <Chip 
+                <Chip
                     // eslint-disable-next-line react/no-array-index-key
-                    key={def.word + i} 
-                    label={`Definition: ${def.word} ${def.phonetic ? `[${def.phonetic}]` : ""}`} 
-                    color="secondary" 
+                    key={def.word + i}
+                    label={`Definition: ${def.word} ${def.phonetic ? `[${def.phonetic}]` : ""}`}
+                    color="secondary"
                     onClick={() => onFreeDictionaryWordClick(freeDefinitions)}
-                    sx={{ mr: 1, mb: 1 }} 
+                    sx={{ mr: 1, mb: 1 }}
                 />
             ))}
 
             {
                 urbanDictionaryDefinitions?.slice(0, 1)?.map((def, i) => (
-                    <Chip 
+                    <Chip
                         // eslint-disable-next-line react/no-array-index-key
-                        key={def.word + i} 
-                        label={`Urban Dictionary: ${def.word}`} 
-                        color="info" 
+                        key={def.word + i}
+                        label={`Urban Dictionary: ${def.word}`}
+                        color="info"
                         onClick={() => onUrbanDictionaryWordClick(urbanDictionaryDefinitions)}
-                        sx={{ mr: 1, mb: 1 }} 
+                        sx={{ mr: 1, mb: 1 }}
                     />
                 ))
             }
